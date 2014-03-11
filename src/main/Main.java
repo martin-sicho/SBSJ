ca362cad1d6b1c14569c0b420896714235407bdc
Martin Sicho <sichom@vscht.cz>
2014-03-11 19:09:21 +0100

    A few adjustments.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ package main;
  */
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("This is the main method.");
     }
 }
