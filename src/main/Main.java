a62c41fd704b1433d722404f4fa06106e8ab4eea
Martin Sicho <sichom@vscht.cz>
2014-03-11 17:49:12 +0100

    Initial commmit with created project files and .gitignore.

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Martin Sicho on 11.3.14.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("This is the main method.");
+    }
+}
