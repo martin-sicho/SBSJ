d43546fd09b0ccb6ff68fd4b4b66de97452270c5
Martin Sicho <sichom@vscht.cz>
2014-03-18 14:53:26 +0100

    Basically just minor changes and settings adjustments. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #	new file:   .idea/inspectionProfiles/Project_Default.xml #	new file:   .idea/inspectionProfiles/profiles_settings.xml #	modified:   .idea/workspace.xml #	modified:   src/main/Main.java #

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,6 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the main method.");
+        CommandLineObjects parameters = new CommandLineObjects(ArgumentBuilder.build(args));
     }
 }
